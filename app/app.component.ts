e8fa700f91f4afbef320fea6dcce68ee529b1597
Oskar Radon and timmi mcguire <student@epicodus.com>
2016-03-22 09:25:47 -0700

    initial setup working

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
-import {Component} from 'angular2/core';
+import { Component } from 'angular2/core';
 
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Skeleton Angular2 App</h1>
+    <div class="container">
+      <h1>Skeleton Angular2 App!</h1>
+    <div>
   `
 })
-
 export class AppComponent {
 
 }
