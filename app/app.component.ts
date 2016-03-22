3fc10da85bad8cfe68d6555ef695fb91dbd617ee
Oskar Radon and timmi mcguire <timothea.mcguire@gmail.com>
2016-03-22 10:03:48 -0700

    Add Tasks array

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,10 +4,26 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
-    <div>
+      <h1>To-Do List</h1>
+      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+    </div>
   `
 })
 export class AppComponent {
+  public tasks: Task[];  // Task[] (or Array<Task>) identifies tasks as an array of Task objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app.", 0),
+      new Task("Learn Kung Fu", 1),
+      new Task("Rewatch all the Lord of the Rings movies.", 2),
+      new Task("Do the laundry.", 3)
+    ];
+  }
+}
 
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number) {
+
+  }
 }
