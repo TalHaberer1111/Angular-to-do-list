59b54fafd13350a6d366a862d3399d2c2c464bae
Oskar Radon and timmi mcguire <oskarradonkimball@gmail.com>
2016-03-22 10:53:28 -0700

    Add taskList component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from 'angular2/core';
 
 @Component({
+  selector: 'task-list',
+  inputs: ['taskList'],
+  template: `
+  <h3 *ngFor="#currentTask of taskList" (click)="taskClicked(currentTask)">
+    {{ currentTask.description }}
+  </h3>
+  `
+})
+export class TaskListComponent {
+  public taskList: Task[];
+  taskClicked(clickedTask: Task): void {
+    console.log(clickedTask);
+  }
+}
+
+@Component({
   selector: 'my-app',
+  directives: [TaskListComponent],
   template: `
     <div class="container">
       <h1>To-Do List</h1>
-      <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+      <task-list [taskList]="tasks"></task-list>
     </div>
   `
 })
@@ -19,11 +36,15 @@ export class AppComponent {
       new Task("Do the laundry.", 3)
     ];
   }
+  taskWasSelected(clickedTask: Task): void{
+    console.log(clickedTask);
+  }
 }
 
-export class Task {
-  public done: boolean = false;
-  constructor(public description: string, public id: number) {
+// model
+  export class Task {
+    public done: boolean = false;
+    constructor(public description: string, public id: number) {
 
   }
 }
